489a598cc391a9b909b0c0736e725acdb64d6da6
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2007-02-28 17:27:16 +0000

    Created a package for control flow tasks;  added IfTask.
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@30 10bdbbbe-7829-0410-8c74-2bcda85da5b1

---
+++ src/org/danann/cernunnos/flow/IfTask.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2007 Andrew Wills
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.danann.cernunnos.flow;
+
+import org.danann.cernunnos.AbstractContainerTask;
+import org.danann.cernunnos.EntityConfig;
+import org.danann.cernunnos.Formula;
+import org.danann.cernunnos.Phrase;
+import org.danann.cernunnos.Reagent;
+import org.danann.cernunnos.ReagentType;
+import org.danann.cernunnos.SimpleFormula;
+import org.danann.cernunnos.SimpleReagent;
+import org.danann.cernunnos.TaskRequest;
+import org.danann.cernunnos.TaskResponse;
+
+public class IfTask extends AbstractContainerTask {
+
+	// Instance Members.
+	private Phrase test;
+
+	/*
+	 * Public API.
+	 */
+
+	public static final Reagent TEST = new SimpleReagent("TEST", "@test", ReagentType.PHRASE, String.class, 
+				"Boolean value indicating whether SUBTASKS should be executed.");
+
+	public Formula getFormula() {
+		Reagent[] reagents = new Reagent[] {TEST, AbstractContainerTask.SUBTASKS};
+		final Formula rslt = new SimpleFormula(IfTask.class, reagents);
+		return rslt;
+	}
+
+	public void init(EntityConfig config) {
+
+		super.init(config);		
+
+		// Instance Members.
+		this.test = (Phrase) config.getValue(TEST);
+		
+	}
+
+	public void perform(TaskRequest req, TaskResponse res) {
+		
+		Boolean b = (Boolean) test.evaluate(req, res);
+		
+		if (b.equals(Boolean.TRUE)) {
+			super.performSubtasks(req, res);
+		}
+		
+	}
+	
+}
