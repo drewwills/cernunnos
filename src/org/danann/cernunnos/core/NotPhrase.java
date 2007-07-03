e307c19e4d5870c65da5cefe2f232bb09b314faa
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2007-07-03 16:20:34 +0000

    Added a phrase to invert a boolean value.
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@93 10bdbbbe-7829-0410-8c74-2bcda85da5b1

---
+++ src/org/danann/cernunnos/core/NotPhrase.java
@@ -0,0 +1,44 @@
+package org.danann.cernunnos.core;
+
+import org.danann.cernunnos.EntityConfig;
+import org.danann.cernunnos.Formula;
+import org.danann.cernunnos.Phrase;
+import org.danann.cernunnos.Reagent;
+import org.danann.cernunnos.ReagentType;
+import org.danann.cernunnos.SimpleFormula;
+import org.danann.cernunnos.SimpleReagent;
+import org.danann.cernunnos.TaskRequest;
+import org.danann.cernunnos.TaskResponse;
+
+public class NotPhrase implements Phrase {
+
+	// Instance Members.
+	private Phrase value;
+
+	/*
+	 * Public API.
+	 */
+	
+	public static final Reagent VALUE = new SimpleReagent("VALUE", "descendant-or-self::text()", 
+			ReagentType.PHRASE, Boolean.class, "A phrase that evaluates to a Boolean.");
+
+	public Formula getFormula() {
+		Reagent[] reagents = new Reagent[] {VALUE};
+		return new SimpleFormula(NotPhrase.class, reagents);
+	}
+	
+	public void init(EntityConfig config) {
+
+		// Instance Members.
+		this.value = (Phrase) config.getValue(VALUE);
+
+	}
+
+	public Object evaluate(TaskRequest req, TaskResponse res) {
+
+		Boolean b = (Boolean) value.evaluate(req, res);
+		return !b.booleanValue();
+		
+	}
+
+}
