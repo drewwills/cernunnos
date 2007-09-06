e21d75d868c20f454507cc512cd41e8059882ec4
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2007-09-06 17:16:46 +0000

    Fixed a bug that broke connections based on DataSources.
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@109 10bdbbbe-7829-0410-8c74-2bcda85da5b1

--- src/org/danann/cernunnos/sql/OpenConnectionTask.java
+++ src/org/danann/cernunnos/sql/OpenConnectionTask.java
@@ -18,6 +18,7 @@ package org.danann.cernunnos.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.sql.DataSource;
 
 import org.danann.cernunnos.AbstractContainerTask;
 import org.danann.cernunnos.Formula;
@@ -32,14 +33,15 @@ import org.danann.cernunnos.TaskRequest;
 import org.danann.cernunnos.TaskResponse;
 
 /**
- * <code>Task</code> implementation that opens a JDBC connection and makes it 
- * available to subtasks.  The new connection will automatically be closed when 
- * execution of this task concludes. 
+ * <code>Task</code> implementation that opens a JDBC connection and makes it
+ * available to subtasks.  The new connection will automatically be closed when
+ * execution of this task concludes.
  */
 public final class OpenConnectionTask extends AbstractContainerTask {
-	
+
 	// Instance Members.
 	private Phrase attribute_name;
+	private Phrase data_source;
 	private Phrase driver;
 	private Phrase url;
 	private Phrase username;
@@ -50,75 +52,90 @@ public final class OpenConnectionTask extends AbstractContainerTask {
 	 */
 
 	public static final Reagent ATTRIBUTE_NAME = new SimpleReagent("ATTRIBUTE_NAME", "@attribute-name", ReagentType.PHRASE, String.class,
-								"Optional name under which the new connection will be registered as a request attribute.  If omitted, the name "
-								+ "'SqlAttributes.CONNECTION' will be used.", new LiteralPhrase(SqlAttributes.CONNECTION));
+								"Optional name under which the new connection will be registered as a request attribute.  If omitted, " +
+								"the name 'SqlAttributes.CONNECTION' will be used.", new LiteralPhrase(SqlAttributes.CONNECTION));
+
+	public static final Reagent DATA_SOURCE = new SimpleReagent("DATA_SOURCE", "@data-source", ReagentType.PHRASE, DataSource.class,
+								"Optional DataSource object from which the connection may be opened.  You must provide either " +
+								"DATA_SOURCE or DRIVER, URL, USERNAME, and PASSWORD.", null);
 
 	public static final Reagent DRIVER = new SimpleReagent("DRIVER", "@driver", ReagentType.PHRASE, String.class,
-								"The JDBC driver class to use when opening the connection.");
+								"Optional JDBC driver class name to use when opening the connection.  You must " +
+								"provide either DATA_SOURCE or DRIVER, URL, USERNAME, and PASSWORD.", null);
 
-	public static final Reagent URL = new SimpleReagent("LOCATION", "@url", ReagentType.PHRASE, String.class,
-								"The JDBC connection LOCATION to use when opening the connection.");
+	public static final Reagent URL = new SimpleReagent("URL", "@url", ReagentType.PHRASE, String.class,
+								"Optional JDBC connection URL to use when opening the connection.  You " +
+								"must provide either DATA_SOURCE or DRIVER, URL, USERNAME, and PASSWORD.", null);
 
 	public static final Reagent USERNAME = new SimpleReagent("USERNAME", "@username", ReagentType.PHRASE, String.class,
-								"The username to use when opening the connection.");
+								"Optional username to use when opening the connection.  You must provide either " +
+								"DATA_SOURCE or DRIVER, URL, USERNAME, and PASSWORD.", null);
 
 	public static final Reagent PASSWORD = new SimpleReagent("PASSWORD", "@password", ReagentType.PHRASE, String.class,
-								"The password to use when opening the connection.");
+								"Optional password to use when opening the connection.  You must provide either " +
+								"DATA_SOURCE or DRIVER, URL, USERNAME, and PASSWORD.", null);
 
 	public Formula getFormula() {
-		Reagent[] reagents = new Reagent[] {ATTRIBUTE_NAME, DRIVER, URL, USERNAME, 
-									PASSWORD, AbstractContainerTask.SUBTASKS};
-		final Formula rslt = new SimpleFormula(OpenConnectionTask.class, reagents);
-		return rslt;
+		Reagent[] reagents = new Reagent[] {ATTRIBUTE_NAME, DATA_SOURCE, DRIVER, URL,
+							USERNAME, PASSWORD, AbstractContainerTask.SUBTASKS};
+		return new SimpleFormula(OpenConnectionTask.class, reagents);
 	}
 
 	public void init(EntityConfig config) {
 
-		super.init(config);		
+		super.init(config);
 
 		// Instance Members.
 		this.attribute_name = (Phrase) config.getValue(ATTRIBUTE_NAME);
+		this.data_source = (Phrase) config.getValue(DATA_SOURCE);
 		this.driver = (Phrase) config.getValue(DRIVER);
 		this.url = (Phrase) config.getValue(URL);
 		this.username = (Phrase) config.getValue(USERNAME);
 		this.password = (Phrase) config.getValue(PASSWORD);
-		
+
 	}
 
 	public void perform(TaskRequest req, TaskResponse res) {
 
-		String finalUrl = (String) url.evaluate(req, res);
 		Connection conn = null;
 		try {
 
 			// Open the Connection...
-			Class.forName((String) driver.evaluate(req, res));
-			conn = DriverManager.getConnection(finalUrl, (String) username.evaluate(req, res), 
-													(String) password.evaluate(req, res));
-			
+			if (driver != null && url != null && username != null && password != null) {
+				// Connect Manually...
+				Class.forName((String) driver.evaluate(req, res));
+				conn = DriverManager.getConnection((String) url.evaluate(req, res),
+									(String) username.evaluate(req, res),
+									(String) password.evaluate(req, res));
+			} else {
+				// Try the DataSource...
+				DataSource ds = (DataSource) data_source.evaluate(req, res);
+				conn = ds.getConnection();
+			}
+
 			// Make it available as a request attribute...
 			res.setAttribute((String) attribute_name.evaluate(req, res), conn);
-			
+
 			// Invoke subtasks...
 			super.performSubtasks(req, res);
-			
+
 		} catch (Throwable t) {
-			String msg = "Unable to connect to the specified database:  " + finalUrl;
+			String msg = "Unable to connect to the specified database";
 			throw new RuntimeException(msg, t);
 		} finally {
-			
+
 			// Cleanup...
 			if (conn != null) {
 				try {
 					conn.close();
 				} catch (Throwable t) {
-					String msg = "Unable to close the specified connection:  " + finalUrl;
+					String msg = "Unable to close the specified connection";
 					throw new RuntimeException(msg, t);
 				}
 			}
-			
+
 		}
-		
+
 	}
-		
+
 }
