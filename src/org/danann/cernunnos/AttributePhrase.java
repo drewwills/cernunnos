e60fbbdbdd664c59218b8a893f19ca56c37dab53
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2007-09-11 18:13:10 +0000

    Fixed the handling of default values.
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@116 10bdbbbe-7829-0410-8c74-2bcda85da5b1

--- src/org/danann/cernunnos/AttributePhrase.java
+++ src/org/danann/cernunnos/AttributePhrase.java
@@ -18,7 +18,7 @@ package org.danann.cernunnos;
 
 
 /**
- * <code>PhraseComponent</code> implementation that evaluates to an attribute 
+ * <code>PhraseComponent</code> implementation that evaluates to an attribute
  * set on the <code>TaskRequest</code>.
  */
 public final class AttributePhrase implements Phrase {
@@ -26,71 +26,83 @@ public final class AttributePhrase implements Phrase {
 	// Instance Members.
 	private Phrase name;
 	private Phrase dflt;
-	
+
 	/*
 	 * Public API.
 	 */
 
 	/**
-	 * Constructor used by the Cernunnos runtime.  Members will be populated in 
+	 * Constructor used by the Cernunnos runtime.  Members will be populated in
 	 * the <code>init</code> method.
 	 */
 	public AttributePhrase() {
-		this((Phrase)null, (Phrase)null);
-	}
-	
-	/**
-	 * Constructs an <code>AttributePhrase</code> that looks for a request 
-	 * attribute with the specified <code>name</code>.
-	 * 
-	 * @param name A phrase that will evaluate to the name of a request 
-	 * attribute.
-	 */
-	public AttributePhrase(Phrase name) {
-		this(name, null);
+		this((Phrase)null);
 	}
 
 	/**
-	 * Constructs an <code>AttributePhrase</code> that looks for a request 
+	 * Constructs an <code>AttributePhrase</code> that looks for a request
 	 * attribute with the specified <code>name</code>.
-	 * 
+	 *
 	 * @param name The name of a request attribute.
 	 */
 	public AttributePhrase(String name) {
-		this(new LiteralPhrase(name), null);
+		this(new LiteralPhrase(name));
 	}
 
 	/**
-	 * Constructs an <code>AttributePhrase</code> that looks for a request 
-	 * attribute with the specified <code>name</code> but returns the value of 
+	 * Constructs an <code>AttributePhrase</code> that looks for a request
+	 * attribute with the specified <code>name</code>.
+	 *
+	 * @param name A phrase that will evaluate to the name of a request
+	 * attribute.
+	 */
+	public AttributePhrase(Phrase name) {
+		this.name = name;
+		this.dflt = null;	// Signifies none was specified...
+	}
+
+	/**
+	 * Constructs an <code>AttributePhrase</code> that looks for a request
+	 * attribute with the specified <code>name</code> but returns the value of
 	 * <code>dflt</code> if that attribute is not present.
-	 * 
+	 *
 	 * @param name The name of a request attribute.
 	 * @param dflt Alternate value to use if the named attribute is not present.
+	 * @throws IllegalArgumentException If <code>dflt</code> is
+	 * <code>null</code>
 	 */
 	public AttributePhrase(String name, Phrase dflt) {
 		this(new LiteralPhrase(name), dflt);
 	}
-	
+
 	/**
-	 * Constructs an <code>AttributePhrase</code> that looks for a request 
-	 * attribute with the specified <code>name</code> but returns the value of 
+	 * Constructs an <code>AttributePhrase</code> that looks for a request
+	 * attribute with the specified <code>name</code> but returns the value of
 	 * <code>dflt</code> if that attribute is not present.
-	 * 
-	 * @param name A phrase that will evaluate to the name of a request 
+	 *
+	 * @param name A phrase that will evaluate to the name of a request
 	 * attribute.
 	 * @param dflt Alternate value to use if the named attribute is not present.
+	 * @throws IllegalArgumentException If <code>dflt</code> is
+	 * <code>null</code>
 	 */
 	public AttributePhrase(Phrase name, Phrase dflt) {
-		// NB:  Either argument may be null... 
-		this.name = name;
+
+		// Assertions.
+		if (dflt == null) {
+			String msg = "Argument 'dflt' cannot be null.";
+			throw new IllegalArgumentException(msg);
+		}
+
+		this.name = name;	// May be null... is this ok?
 		this.dflt = dflt;
+
 	}
-	
-	public static final Reagent NAME = new SimpleReagent("KEY", "descendant-or-self::text()", ReagentType.PHRASE, 
+
+	public static final Reagent NAME = new SimpleReagent("KEY", "descendant-or-self::text()", ReagentType.PHRASE,
 								String.class, "The name of an attribute defined in the TaskRequest.");
 
-	public static final Reagent DEFAULT = new SimpleReagent("DEFAULT", "@default", ReagentType.PHRASE, String.class, 
+	public static final Reagent DEFAULT = new SimpleReagent("DEFAULT", "@default", ReagentType.PHRASE, String.class,
 								"If specified, this value will be used returned if the named attribute is not "
 								+ "present on the request.  If a DEFAULT is not specified and the named attribute "
 								+ "is not present, an error will occur.", null);
@@ -99,7 +111,7 @@ public final class AttributePhrase implements Phrase {
 		Reagent[] reagents = new Reagent[] {NAME, DEFAULT};
 		return new SimpleFormula(AttributePhrase.class, reagents);
 	}
-	
+
 	public void init(EntityConfig config) {
 
 		// Instance Members.
@@ -111,14 +123,14 @@ public final class AttributePhrase implements Phrase {
 	public Object evaluate(TaskRequest req, TaskResponse res) {
 
 		String n = (String) name.evaluate(req, res);
-		
+
 		// Check to see if the default should be appliead instead...
 		if (dflt != null && !req.hasAttribute(n)) {
 			return dflt.evaluate(req, res);
 		}
-		
+
 		return req.getAttribute(n);
-		
+
 	}
-	
+
 }
