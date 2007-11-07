0b1fc2882287693c628ca9c11761b7dfb96b2b0f
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2007-11-07 22:11:57 +0000

    Added a task to iterate over command line arguments.
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@178 10bdbbbe-7829-0410-8c74-2bcda85da5b1

---
+++ src/org/danann/cernunnos/runtime/ArgumentIteratorTask.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2007 Andrew Wills
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.danann.cernunnos.runtime;
+
+import org.danann.cernunnos.AbstractContainerTask;
+import org.danann.cernunnos.Attributes;
+import org.danann.cernunnos.EntityConfig;
+import org.danann.cernunnos.Formula;
+import org.danann.cernunnos.LiteralPhrase;
+import org.danann.cernunnos.Phrase;
+import org.danann.cernunnos.Reagent;
+import org.danann.cernunnos.ReagentType;
+import org.danann.cernunnos.SimpleFormula;
+import org.danann.cernunnos.SimpleReagent;
+import org.danann.cernunnos.TaskRequest;
+import org.danann.cernunnos.TaskResponse;
+
+public class ArgumentIteratorTask extends AbstractContainerTask {
+
+	// Instance Members.
+	private Phrase attribute_name;
+
+	/*
+	 * Public API.
+	 */
+
+	public static final Reagent ATTRIBUTE_NAME = new SimpleReagent("ATTRIBUTE_NAME", "@attribute-name", ReagentType.PHRASE, String.class,
+					"Optional name under which each argument will be registered as a request attribute.  If omitted, " +
+					"the name 'Attributes.STRING' will be used.", new LiteralPhrase(Attributes.STRING));
+
+	public Formula getFormula() {
+		Reagent[] reagents = new Reagent[] {ATTRIBUTE_NAME, AbstractContainerTask.SUBTASKS};
+		final Formula rslt = new SimpleFormula(ArgumentIteratorTask.class, reagents);
+		return rslt;
+	}
+
+	public void init(EntityConfig config) {
+
+		// Instance Members.
+		this.attribute_name = (Phrase) config.getValue(ATTRIBUTE_NAME);
+
+		super.init(config);
+
+	}
+
+
+	public void perform(TaskRequest req, TaskResponse res) {
+
+		String attr = (String) attribute_name.evaluate(req, res);
+		for (int i=1; req.hasAttribute("$"+i); i++) {
+			res.setAttribute(attr, req.getAttribute("$"+i));
+			super.performSubtasks(req, res);
+		}
+	}
+}
