88ae47c019d21edfefc5855ebf9c6eb7c00a1393
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2007-11-15 04:07:35 +0000

    Minor imporvements to logging from the command line.
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@184 10bdbbbe-7829-0410-8c74-2bcda85da5b1

--- bin/crn.sh
+++ bin/crn.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ## Copyright 2007 Andrew Wills
-## 
+##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
 ## You may obtain a copy of the License at
-## 
+##
 ##     http://www.apache.org/licenses/LICENSE-2.0
-## 
+##
 ## Unless required by applicable law or agreed to in writing, software
 ## distributed under the License is distributed on an "AS IS" BASIS,
 ## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,13 +16,13 @@
 ##
 ## Establish the classpath & script we expext to use...
 
-CRN_CLASSPATH=$CRN_HOME/build
+CRN_CLASSPATH=$CRN_HOME/build:$CRN_HOME/config
 
 for f in $(ls $CRN_HOME/lib/*.jar); do
     CRN_CLASSPATH=${CRN_CLASSPATH}:$f
 done
 
-## 
+##
 CRN_ARGS=$*
 
 java -cp "$CRN_CLASSPATH" org.danann.cernunnos.runtime.Main $CRN_ARGS
