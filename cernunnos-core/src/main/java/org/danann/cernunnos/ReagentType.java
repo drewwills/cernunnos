043817a430941e96f634ecab2266328a6950881f
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2008-09-10 00:30:44 +0000

    Issue #44:  ReagentType is improperly evaluating 'descendant-or-self::text()' against an empty TEXT node
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@372 10bdbbbe-7829-0410-8c74-2bcda85da5b1

--- cernunnos-core/src/main/java/org/danann/cernunnos/ReagentType.java
+++ cernunnos-core/src/main/java/org/danann/cernunnos/ReagentType.java
@@ -33,110 +33,128 @@ import org.dom4j.XPath;
  */
 public enum ReagentType {
 
-	/*
-	 * Public API.
-	 */
+    /*
+     * Public API.
+     */
 
-	/**
-	 * Use <code>ReagentType.PHRASE</code> in most cases.  This type allows the
-	 * final value to be calculated using <code>PhraseComponent</code>
-	 * implementations, which provide tremendous flexibility.
-	 */
-	PHRASE {
-	    @Override
+    /**
+     * Use <code>ReagentType.PHRASE</code> in most cases.  This type allows the
+     * final value to be calculated using <code>PhraseComponent</code>
+     * implementations, which provide tremendous flexibility.
+     */
+    PHRASE {
+        @Override
         public Object evaluate(Grammar grammar, Node src, XPath xpath) {
 
-			// Assertions...
-			if (grammar == null) {
-				String msg = "Argument 'grammar' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
-			if (src == null) {
-				String msg = "Argument 'src' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
-			if (xpath == null) {
-				String msg = "Argument 'xpath' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
+            // Assertions...
+            if (grammar == null) {
+                String msg = "Argument 'grammar' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
+            if (src == null) {
+                String msg = "Argument 'src' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
+            if (xpath == null) {
+                String msg = "Argument 'xpath' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
 
-			final Node match = xpath.selectSingleNode(src);
-			return match != null ? grammar.newPhrase(match) : null;
+            Node match = xpath.selectSingleNode(src);
+			
+            // We're obligated to intercept empty 
+            // TEXT nodes and treat them as null...
+            if (match != null && match.getNodeType() == Node.TEXT_NODE 
+			                        && match.getText().length() == 0) {
+                match = null;
+            }
+			
+            return match != null ? grammar.newPhrase(match) : null;
 
-		}
-	},
+        }
+    },
 
-	/**
-	 * Use <code>ReagentType.STRING</code> when the final value of the
-	 * <code>SimpleReagent</code> is used (or must be known) at bootsrap time.
-	 */
-	STRING {
-	    @Override
+    /**
+     * Use <code>ReagentType.STRING</code> when the final value of the
+     * <code>SimpleReagent</code> is used (or must be known) at bootstrap time.
+     */
+    STRING {
+        @Override
         public Object evaluate(Grammar grammar, Node src, XPath xpath) {
 
-			// Assertions...
-			if (grammar == null) {
-				String msg = "Argument 'grammar' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
-			if (src == null) {
-				String msg = "Argument 'src' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
-			if (xpath == null) {
-				String msg = "Argument 'xpath' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
+            // Assertions...
+            if (grammar == null) {
+                String msg = "Argument 'grammar' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
+            if (src == null) {
+                String msg = "Argument 'src' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
+            if (xpath == null) {
+                String msg = "Argument 'xpath' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
 
-			final String value = xpath.valueOf(src).trim();	// do we need to trim?
-			return value.length() > 0 ? value : null;
+            final String value = xpath.valueOf(src).trim();	// do we need to trim?
+            return value.length() > 0 ? value : null;
 
-		}
-	},
+        }
+    },
 
-	/**
-	 * Use <code>ReagentType.NODE_LIST</code> when the final value of the
-	 * <code>SimpleReagent</code> is an XML structure.
-	 */
-	NODE_LIST {
-		@Override
+    /**
+     * Use <code>ReagentType.NODE_LIST</code> when the final value of the
+     * <code>SimpleReagent</code> is an XML structure.
+     */
+    NODE_LIST {
+        @Override
         public Object evaluate(Grammar grammar, Node src, XPath xpath) {
 
-			// Assertions...
-			if (grammar == null) {
-				String msg = "Argument 'grammar' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
-			if (src == null) {
-				String msg = "Argument 'src' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
-			if (xpath == null) {
-				String msg = "Argument 'xpath' cannot be null.";
-				throw new IllegalArgumentException(msg);
-			}
+            // Assertions...
+            if (grammar == null) {
+                String msg = "Argument 'grammar' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
+            if (src == null) {
+                String msg = "Argument 'src' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
+            if (xpath == null) {
+                String msg = "Argument 'xpath' cannot be null.";
+                throw new IllegalArgumentException(msg);
+            }
 
-			// The following fancy conversion is here to avoid type safety warnings..
-			final List<?> matches = xpath.selectNodes(src);
-			final List<Node> rslt = new LinkedList<Node>();
-			for (final Iterator<?> it = matches.iterator(); it.hasNext();) {
-				rslt.add((Node) it.next());
-			}
-			return rslt.size() != 0 ? rslt : null;
+            // The following fancy conversion is here to avoid type safety warnings..
+            final List<?> matches = xpath.selectNodes(src);
+            final List<Node> rslt = new LinkedList<Node>();
 
-		}
-	};
+            for (final Iterator<?> it = matches.iterator(); it.hasNext();) {
+                Node match = (Node) it.next();
+                // We're obligated to intercept empty 
+                // TEXT nodes and treat them as null...
+                if (match != null && !(match.getNodeType() == Node.TEXT_NODE 
+	                                    && match.getText().length() == 0)) {
+                    rslt.add(match);
+                }
+            }
 
-	/**
-	 * Calculates the value of a <code>SimpleReagent</code> given the specified
-	 * <code>Grammar</code>, XML, and XPath expression.
-	 *
-	 * @param grammar The currently governing <code>Grammar</code> instance.
-	 * @param src An XML structure that defines a task.
-	 * @param xpath An expression that will be evaluated on the <code>src</code>
-	 * node to calculate the value of a <code>SimpleReagent</code>.
-	 * @return A reagent or <code>null</code>.
-	 */
-	public abstract Object evaluate(Grammar grammar, Node src, XPath xpath);
+            return rslt.size() != 0 ? rslt : null;
+
+        }
+    };
+
+    /**
+     * Calculates the value of a <code>Reagent</code> given the specified
+     * <code>Grammar</code>, XML, and <code>XPath</code>.  This method returns 
+     * <code>null</null> if either (1) the XPath expression matches nothing or 
+     * (2) the XPath expression matches an empty <code>TEXT</code> node. 
+     *
+     * @param grammar The currently governing <code>Grammar</code> instance
+     * @param src An XML structure that defines a task
+     * @param xpath A pre-compiled <code>XPath</code> object
+     * node to calculate the value of a <code>SimpleReagent</code>
+     * @return A reagent or <code>null</code>
+     */
+    public abstract Object evaluate(Grammar grammar, Node src, XPath xpath);
 
 }
