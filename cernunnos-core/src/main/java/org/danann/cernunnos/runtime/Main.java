cc5dd115f98daf68e67b936e66cdbfed6f7dffc0
wills.drew <wills.drew@10bdbbbe-7829-0410-8c74-2bcda85da5b1>
2009-02-18 16:37:37 +0000

    Issue #76:  Enhance the shell tool to run Cernunnos XML in the CRN_HOME\bin directory from anywhere
    git-svn-id: https://cernunnos.googlecode.com/svn/trunk@446 10bdbbbe-7829-0410-8c74-2bcda85da5b1

--- cernunnos-core/src/main/java/org/danann/cernunnos/runtime/Main.java
+++ cernunnos-core/src/main/java/org/danann/cernunnos/runtime/Main.java
@@ -16,6 +16,7 @@
 
 package org.danann.cernunnos.runtime;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
@@ -27,19 +28,62 @@ public final class Main {
 
 	public static void main(String[] args) {
 
-		// Put some whitespace between the command and the output...
+        Log log = LogFactory.getLog(ScriptRunner.class);
+
+        // Put some whitespace between the command and the output...
 		System.out.println("");
 
 		// Register custom protocol handlers...
 		try {
 			URL.setURLStreamHandlerFactory(new URLStreamHandlerFactoryImpl());
 		} catch (Throwable t) {
-			Log log = LogFactory.getLog(ScriptRunner.class);
 			log.warn("Cernunnos was unable to register a URLStreamHandlerFactory.  " +
 					"Custom URL protocols may not work properly (e.g. classpath://, c:/).  " +
 					"See stack trace below.", t);
 		}
-
+		
+		// Establish CRN_HOME...
+		String crnHome = System.getenv("CRN_HOME");
+		if (crnHome == null) {
+		    if (log.isDebugEnabled()) {
+	            log.debug("The CRN_HOME environment variable is not defined;  " +
+	            		"this is completely normal for embedded applications " +
+	            		"of Cernunnos.");
+		    }
+		}
+		
+		// Look at the specified script:  might it be in the bin/ directory?
+		String location = args[0];
+		try {
+		    // This is how ScriptRunner will attempt to locate the script file...
+	        URL u = new URL(new File(".").toURI().toURL(), location);
+	        if (u.getProtocol().equals("file")) {
+	            // We know the specified resource is a local file;  is it either 
+	            // (1) absolute or (2) relative to the directory where Java is 
+	            // executing?
+	            if (!new File(u.toURI()).exists() && crnHome != null) {
+	                // No.  And what's more, 'CRN_HOME' is defined.  In this case 
+	                // let's see if the specified script *is* present in the bin/ 
+	                // directory.
+	                StringBuilder path = new StringBuilder();
+	                path.append(crnHome).append(File.separator).append("bin")
+	                            .append(File.separator).append(location);
+	                File f = new File(path.toString());
+	                if (f.exists()) {
+	                    // The user is specifying a Cernunnos script in the bin/ directory...
+	                    location = f.toURI().toURL().toExternalForm();
+	                    if (log.isInfoEnabled()) {
+	                        log.info("Resolving the specified Cernunnos document " +
+	                                "to a file in the CRN_HOME/bin directory:  " + 
+	                                location);
+	                    }
+	                }
+	            }
+	        }
+        } catch (Throwable t) {
+            // Just let this pass -- genuine issues will be caught & reported shortly...
+        }
+        
 		// Analyze the command-line arguments...
 		RuntimeRequestResponse req = new RuntimeRequestResponse();
 		switch (args.length) {
@@ -56,7 +100,7 @@ public final class Main {
 		}
 
 		ScriptRunner runner = new ScriptRunner();
-		runner.run(args[0], req);
+		runner.run(location, req);
 
 	}
 
